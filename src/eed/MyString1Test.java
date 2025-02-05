27e6fdbbb99b50a29265e4525be71651b8bd526a
daniguti99 <dgm1021999@gmail.com>
2025-02-05 13:38:06 +0100

    first commit

---
+++ src/eed/MyString1Test.java
@@ -0,0 +1,63 @@
+package eed;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+class MyString1Test {
+
+	@Test
+	void testContarPalabras() {
+			assertEquals(2,MyString1.contarPalabras("Mi madre"));
+			assertEquals(0,MyString1.contarPalabras(""));
+			assertEquals(4,MyString1.contarPalabras("Mi madre es rubia"));
+			assertNotEquals(4,MyString1.contarPalabras(null));
+			assertEquals(4,MyString1.contarPalabras(" Mi madre es rubia"));
+	}
+
+	@Test
+	void testContarLetrasDiferentes() {
+			assertEquals(4,MyString1.contarLetrasDiferentes("hola"));
+			assertEquals(6,MyString1.contarLetrasDiferentes("Mi madre"));
+			assertEquals(5,MyString1.contarLetrasDiferentes("Padre"));
+			assertEquals(0,MyString1.contarLetrasDiferentes(""));
+			assertEquals(0,MyString1.contarLetrasDiferentes(null));
+			assertEquals(5,MyString1.contarLetrasDiferentes(" Padre"));
+			assertEquals(1,MyString1.contarLetrasDiferentes("ñ"));
+	}
+
+	@Test
+	void testContarFrecuenciaPalabras() {
+		Map<String, Integer> frecuenciaPalabras = new HashMap<>();
+		
+		assertTrue(Map.of("mapa",3,"blanco", 1).equals(MyString1.contarFrecuenciaPalabras("blanco mapa mapa mapa")));
+		assertTrue(frecuenciaPalabras.equals(MyString1.contarFrecuenciaPalabras("")));
+		assertTrue(frecuenciaPalabras.equals(MyString1.contarFrecuenciaPalabras(null)));
+	}
+
+	@Test
+	void testContarFrecuenciaLetras() {
+		Map<Character, Integer> frecuenciaLetras = new HashMap<>();
+		
+		assertTrue(Map.of('i',3).equals(MyString1.contarFrecuenciaLetras("iii")));
+		assertTrue(frecuenciaLetras.equals(MyString1.contarFrecuenciaLetras("")));
+		assertTrue(frecuenciaLetras.equals(MyString1.contarFrecuenciaLetras(null)));
+		
+		
+	        
+	}
+
+	@Test
+	void testEsPalindroma() {
+		assertTrue(MyString1.esPalindroma("reconocer"));
+		assertFalse(MyString1.esPalindroma("futbol"));
+		assertFalse(MyString1.esPalindroma(null));
+		assertFalse(MyString1.esPalindroma(""));
+	}
+
+}
+
+
